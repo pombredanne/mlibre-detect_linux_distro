c5afa36a2276b885aecb28965d9042d2eb20118a
mlibre <m.gh@linuxmail.org>
2016-02-12 19:34:42 +0330

    hope

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(file_name):
 
 setup(
 	name = "detect_linux_distro",
-	version = "0.5",
+	version = "0.9",
 	author_email = "m.gh@linuxmail.org",
 	description = "Detect linux distribution",
 	long_description = read("readme"),
